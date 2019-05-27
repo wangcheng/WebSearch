5906def331622226a515223e5a6d5ef99c4447b5
Cheng Wang <wangcheng678@gmail.com>
2019-05-27 22:38:36 +0800

    style: reformat code

--- app/src/main/java/io/github/wangcheng678/websearch/SearchActivity.java
+++ app/src/main/java/io/github/wangcheng678/websearch/SearchActivity.java
@@ -7,37 +7,37 @@ import android.net.Uri;
 import android.os.Bundle;
 
 public class SearchActivity extends Activity {
-  static Intent getSearchIntent(String query) {
-    Uri urlFromQuery = Uri.parse(query);
-    if(urlFromQuery.isAbsolute()){
-      return new Intent(Intent.ACTION_VIEW, urlFromQuery);
-    }
-    Uri.Builder builder = new Uri.Builder();
-    Uri url = builder.scheme("https").authority("www.google.com").appendPath("search").appendQueryParameter("q", query).build();
-    return new Intent(Intent.ACTION_VIEW, url);
-  }
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    Intent intent = getIntent();
-    String action = intent.getAction();
-    String query;
-    if (Intent.ACTION_PROCESS_TEXT.equals(action)) {
-      query = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString();
-    } else if (Intent.ACTION_SEARCH.equals(action) || Intent.ACTION_WEB_SEARCH.equals(action)) {
-      query = intent.getStringExtra(SearchManager.QUERY);
-    } else {
-      finish();
-      return;
+    static Intent getSearchIntent(String query) {
+        Uri urlFromQuery = Uri.parse(query);
+        if (urlFromQuery.isAbsolute()) {
+            return new Intent(Intent.ACTION_VIEW, urlFromQuery);
+        }
+        Uri.Builder builder = new Uri.Builder();
+        Uri url = builder.scheme("https").authority("www.google.com").appendPath("search").appendQueryParameter("q", query).build();
+        return new Intent(Intent.ACTION_VIEW, url);
     }
 
-    Intent resultIntent = getSearchIntent(query);
-    if (resultIntent.resolveActivity(getPackageManager()) != null) {
-      startActivity(resultIntent);
-    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    finish();
-  }
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String query;
+        if (Intent.ACTION_PROCESS_TEXT.equals(action)) {
+            query = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString();
+        } else if (Intent.ACTION_SEARCH.equals(action) || Intent.ACTION_WEB_SEARCH.equals(action)) {
+            query = intent.getStringExtra(SearchManager.QUERY);
+        } else {
+            finish();
+            return;
+        }
+
+        Intent resultIntent = getSearchIntent(query);
+        if (resultIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(resultIntent);
+        }
+
+        finish();
+    }
 }
